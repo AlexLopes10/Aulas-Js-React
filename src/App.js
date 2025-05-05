f4101d1ff2cd81098db753db6a892708dfdd764e
AlexLopes10 <alexlopes100221@gmail.com>
2025-05-05 14:23:29 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Teste, Click } from './components/Utils';
 function App() {
   return (
     <form>
-      <input onChange={(e) => { Teste(e.target.value,'--é muito Legal--') }} />
+      <input onChange={(e) => { Teste(e.target.value) }} />
       <button onClick={(e) => { Click('Alex','Legal') }}>Enviar</button>
     </form>
   );
